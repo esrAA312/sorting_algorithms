1113f900a48d68fb0455bea97f8fd88b6fdd5a04
yusrA312 <yusra2yahya@gmail.com>
2024-01-20 03:11:15 -0800

    done

--- tasks/1000-sort_deck.c
+++ tasks/1000-sort_deck.c
@@ -59,17 +59,14 @@ void sort_deck(deck_node_t **deck)
  */
 int _strcmp(const char *s1, const char *s2)
 {
-	int i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		if (s1[i] != s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		i++;
+		s1++;
+		s2++;
 	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
 	return (0);
 }
 
